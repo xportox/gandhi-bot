a096dfbbf295988e5e124fc6980154404dfbb795
Rodrigo Porto <xportox@protonmail.com>
2021-03-05 22:18:58 -0300

    Namastê

---
+++ commands/namaste.js
@@ -0,0 +1,11 @@
+exports.run = (bot, message) => {
+  bot.sendText(message.from, "🙏 namaskar, camarada");
+};
+
+exports.help = {
+  name: "Namastê",
+  description:
+    "Que todos os seres vivos da terra possam se livrar dos sofrimentos e sentir apenas paz e tranquilidade",
+  usage: "namaste",
+  cooldown: 5,
+};
